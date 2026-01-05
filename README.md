51418d24764d672ca90e1e8934c2f4541df28578
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2026-01-05 22:48:47 +0900

    ScrollInput実装

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # BlazorUtil
+
+多少なりとも工数がかかったものを上げていきます
+
+質問等あればQiitaか kobayashi.50po100po@gmail.com まで
+
+## How To Use
+
+### 依存関係注入
+
+Program.cs または MauiProgram.cs に
+```C#
+builder.Services.AddScoped<JSInterop>();
+```
+を追加
+
+これでJSが動かせます(ないと例外出ます)
+
+### Bootstrap
+
+BlazorはデフォルトでBootstrapが適用されているので不要？
+
+```
+<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" />
+<script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+```
+
+もしなかったら、JSも追加してください  
+当然ですが、パスは自分の環境を確認してください
---
+++ src/BlazorUtil/JSInterop.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorUtil
+{
+    public class JSInterop
+    {
+        private IJSRuntime _jSRuntime;
+
+        public JSInterop(IJSRuntime jsRuntime)
+        {
+            _jSRuntime = jsRuntime;
+        }
+
+        public async Task<Module> GetModule(string moduleName)
+        {
+            return new(await _jSRuntime.InvokeAsync<IJSObjectReference>("import", $"./_content/BlazorUtil/js/{moduleName}.js"));
+        }
+    }
+
+    public class Module : IAsyncDisposable
+    {
+        private IJSObjectReference _jSObjectReference;
+
+        public Module(IJSObjectReference jSObjectReference)
+        {
+            _jSObjectReference = jSObjectReference;
+        }
+
+        public async ValueTask CallScript(string method, params object[] args)
+        {
+            await _jSObjectReference.InvokeVoidAsync(method, args);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _jSObjectReference.DisposeAsync();
+        }
+    }
+}
